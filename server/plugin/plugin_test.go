6c74e916a1fa64e93e34416665e6b9cdefa766d0
kaakaa <stooner.hoe@gmail.com>
2020-05-05 11:02:07 +0900

    Fix plugin test

--- server/plugin/plugin_test.go
+++ server/plugin/plugin_test.go
@@ -6,16 +6,27 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestServeHTTP(t *testing.T) {
 	assert := assert.New(t)
-	plugin := SharePostPlugin{}
+	p := SharePostPlugin{}
+
+	api := &plugintest.API{}
+	api.On("LogDebug", GetMockArgumentsWithType("string", 7)...).Return()
+	defer api.AssertExpectations(t)
+	p.SetAPI(api)
+
+	p.router = p.InitAPI()
+	p.setConfiguration(&configuration{})
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/", nil)
 
-	plugin.ServeHTTP(nil, w, r)
+	p.ServeHTTP(nil, w, r)
 
 	result := w.Result()
 	defer result.Body.Close()
@@ -25,5 +36,13 @@ func TestServeHTTP(t *testing.T) {
 	assert.Nil(err)
 	bodyString := string(bodyBytes)
 
-	assert.Equal("Hello, world!", bodyString)
+	assert.Equal("Installed SharePostPlugin v0.1.0", bodyString)
+}
+
+func GetMockArgumentsWithType(typeString string, num int) []interface{} {
+	ret := make([]interface{}, num)
+	for i := 0; i < len(ret); i++ {
+		ret[i] = mock.AnythingOfTypeArgument(typeString)
+	}
+	return ret
 }
